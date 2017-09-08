ee72fdcfb3c0f2d69061a0f1f0646854ddd27949
kwc2015 <khaiwah@gmail.com>
2017-09-08 13:28:39 +0200

    Add project files.

---
+++ EventsAndDelegatesDemo/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Testing Events and Delegates.
+//By Khai Wah Cheung. 08.09.2017.
+
+namespace EventsAndDelegatesDemo
+{
+    public class Person
+    {
+        string name;
+
+        public string Name{
+            get => name;
+            set => name = value;
+        }
+
+        public void changeName(string n)
+        {
+            string oldName = name;
+            this.name = n;
+            Console.WriteLine("@Person: Name changed from {0} to {1}", oldName, n);
+
+            //raising the event
+            OnNameChanged();
+        }
+
+        //Built-in delegate
+        public event EventHandler NameChanged;
+
+        protected virtual void OnNameChanged()
+        {
+            //checking if there are any subscribers
+            if (NameChanged != null) 
+            {
+                NameChanged(this, EventArgs.Empty);
+            }
+        }
+
+    }
+
+    class Goverment {
+        public void OnNameChanged(object source, EventArgs e)
+        {
+            Console.WriteLine("@Goverment: Received name change event.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person() { Name = "Khai Wah Cheung" };
+            var goverment = new Goverment();
+
+            //subscription
+            person.NameChanged += goverment.OnNameChanged;
+
+            person.changeName("John Petrucci");
+        }
+    }
+}
